bbd76c321d91f643425e0641d5066334eee12130
Billy Chan <ccw.billy.123@gmail.com>
2023-02-23 16:00:50 +0800

    Fix test case

--- core/tests/mock.rs
+++ core/tests/mock.rs
@@ -8,7 +8,7 @@ async fn main() {
   let db = &prepare_mock_db();
 
   {
-    let page = Query::find_address_in_page(db, "elektr".to_owned(), 0, 1).await;
+    let page = Query::find_address_in_page(db, vec!["elektr".to_owned()], 1, 1).await;
     let page_unwrapped = page.unwrap();
     println!("{:?}", page_unwrapped.0);
     assert!(true)
